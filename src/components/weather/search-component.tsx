af00b34dfef4f09cafa7be69abd54d58e63e8a23
masoumeh kianfar <masoume.kianfar.mc@gmail.com>
2026-07-09 13:44:57 +0330

    fix search component

--- src/components/weather/search-component.tsx
+++ src/components/weather/search-component.tsx
@@ -1,24 +1,70 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { City } from "country-state-city";
 
 interface SearchWeatherComponentProps {
   text: string;
   setText: Dispatch<SetStateAction<string>>;
-  weatherHandler: () => void;
+  setData: any | null;
+  setchosenCity: Dispatch<SetStateAction<string>>;
 }
 
 const SearchWeatherComponent = ({
   text,
   setText,
-  weatherHandler,
+  setData,
+  setchosenCity,
 }: SearchWeatherComponentProps) => {
+  const [menu, setMenu] = useState<boolean>(false);
+  const [cities, setCities] = useState<string[]>([]);
+
   const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
+    setMenu(true);
+  };
+
+  useEffect(() => {
+    const allCities = City.getAllCities();
+    const citiesName = allCities.map((city) => city.name);
+    setCities(citiesName);
+  }, []);
+
+  const chosenCityHandler = (city: string) => {
+    setText(city);
+    setMenu(false);
+    setchosenCity(city);
+  };
+
+  const resetSearch = () => {
+    setText("");
+    setMenu(false);
+    setchosenCity("");
+    setData(null);
   };
 
   return (
     <div>
-      <input value={text} onChange={searchHandler} />
-      <button onClick={weatherHandler}>search</button>
+      <div style={{position:"relative", width:"200px"}}>
+        <input value={text} onChange={searchHandler} minLength={2} style={{width:"100%",height:"24px",padding:"0 12px 0 4px"}} />
+        {text &&<span style={{position:"absolute",right:"4px", top:"4px",zIndex:5}} onClick={resetSearch}>*</span>}
+      </div>
+
+      {menu && text.length > 2 && (
+        <div>
+          {!cities ? (
+            <div>loading</div>
+          ) : (
+            cities
+              .filter((city) =>
+                city.toLowerCase().startsWith(text.toLowerCase()),
+              )
+              .map((city, index) => (
+                <li onClick={() => chosenCityHandler(city)} key={index}>
+                  {city}
+                </li>
+              ))
+          )}
+        </div>
+      )}
     </div>
   );
 };
