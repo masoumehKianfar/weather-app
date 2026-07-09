12afc9a87b04d06f248d989c3cc8847f53a98bfd
masoumeh kianfar <masoume.kianfar.mc@gmail.com>
2026-07-09 13:45:29 +0330

    fix weather component

--- src/components/weather/weather-component.tsx
+++ src/components/weather/weather-component.tsx
@@ -1,46 +1,64 @@
 "use client";
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 import SearchWeatherComponent from "./search-component";
 
 const WeatherComponent = () => {
   const [text, setText] = useState("");
+  const [chosenCity, setchosenCity] = useState<string>("");
+  const [data, setData] = useState<any | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const [data, setData] = useState<any>(null);
   const apiDetails = {
     base: process.env.NEXT_PUBLIC_BASE_URL,
     key: process.env.NEXT_PUBLIC_API_kEY,
   };
 
-    const weatherHandler = async () => {
-      try {
-        const response = await fetch(
-          `${apiDetails.base}weather?q=${text}&appid=${apiDetails.key}&units=metric`,
-        );
+  const weatherHandler = async () => {
+    if (!chosenCity) return;
 
-        if (!response.ok) {
-          throw new Error("failed to fetch");
-        }
+    setLoading(true);
 
-        const result = await response.json();
-        console.log("Here is our weather data: 📦", result);
-        setData(result);
-      } catch (error: any) {
-        console.log("Oops! an error occurred!", error.message);
+    try {
+      const response = await fetch(
+        `${apiDetails.base}weather?q=${chosenCity}&appid=${apiDetails.key}&units=metric`,
+      );
+
+      if (!response.ok) {
+        throw new Error("failed to fetch");
       }
-    };
+
+      const result = await response.json();
+      setData(result);
+    } catch (error: any) {
+      console.log("Oops! an error occurred!", error.message);
+    } finally{
+      setLoading(false)
+    }
+  };
+
+  useEffect(() => {
+    weatherHandler();
+  }, [chosenCity]);
 
   return (
     <div>
-      <SearchWeatherComponent text={text} setText={setText} weatherHandler={weatherHandler}/>
+      <SearchWeatherComponent
+        text={text}
+        setText={setText}
+        setchosenCity={setchosenCity}
+        setData={setData}
+      />
 
       <div>
-        {data && (
+        {data ? (
           <div>
             <h3>{data.name}</h3>
             <p>{Math.round(data.main.temp)}°C</p>
             <p>{data.weather[0].description}</p>
           </div>
-        ) }
+        ) : loading ? (
+          <div>Loading</div>
+        ) : null}
       </div>
     </div>
   );
