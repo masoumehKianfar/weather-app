e7b1b4ddead1459ecfdf51bce630e00db3740465
masoumeh kianfar <masoume.kianfar.mc@gmail.com>
2026-07-18 17:49:06 +0330

    fix search-component,install iconify package and use iconify

--- src/components/weather/search-component.tsx
+++ src/components/weather/search-component.tsx
@@ -1,5 +1,6 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { City } from "country-state-city";
+import { Icon } from "@iconify/react";
 
 interface SearchWeatherComponentProps {
   text: string;
@@ -29,7 +30,7 @@ const SearchWeatherComponent = ({
   }, []);
 
   const chosenCityHandler = (city: string) => {
-    setText(city);
+    setText("");
     setMenu(false);
     setchosenCity(city);
   };
@@ -43,28 +44,39 @@ const SearchWeatherComponent = ({
 
   return (
     <div>
-      <div style={{position:"relative", width:"200px"}}>
-        <input value={text} onChange={searchHandler} minLength={2} style={{width:"100%",height:"24px",padding:"0 12px 0 4px"}} />
-        {text &&<span style={{position:"absolute",right:"4px", top:"4px",zIndex:5}} onClick={resetSearch}>*</span>}
-      </div>
+      <div className="relative w-[300px] h-full">
+        <input
+          value={text}
+          onChange={searchHandler}
+          minLength={2}
+          className="w-full h-[24px] py-2 pr-[12px] pl-[4px] border-2 rounded-sm border-blue-500 text-sm focus:outline-0 focus:border-blue-500 "
+        />
+        {text && (
+          <Icon icon="iconamoon:close-bold"  className="absolute right-[4px] top-[4px] z-50 hover:text-red-500 cursor-pointer "
+            onClick={resetSearch}/>
+        )}
 
-      {menu && text.length > 2 && (
-        <div>
-          {!cities ? (
-            <div>loading</div>
-          ) : (
-            cities
-              .filter((city) =>
-                city.toLowerCase().startsWith(text.toLowerCase()),
-              )
-              .map((city, index) => (
-                <li onClick={() => chosenCityHandler(city)} key={index}>
-                  {city}
-                </li>
-              ))
-          )}
-        </div>
-      )}
+        {menu && text.length > 2 && (
+          <div className="max-h-[300px] overflow-y-auto absolute top-[38px] left-0 min-h-[100px] h-full w-full bg-gray-50/50 backdrop-blur-sm p-2 z-50 rounded-md">
+            {!cities ? (
+              <div>loading</div>
+            ) : (
+              cities
+                .filter((city) =>
+                  city.toLowerCase().startsWith(text.toLowerCase()),
+                )
+                .map((city, index) => (
+                  <li
+                    onClick={() => chosenCityHandler(city)}
+                    key={index}
+                  >
+                    {city}
+                  </li>
+                ))
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
